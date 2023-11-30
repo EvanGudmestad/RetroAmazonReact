79a34aa8c55e31c5c2d4eb0978d82709df987523
EvanGudmestad <eagudmestad@ranken.edu>
2023-11-30 11:40:42 -0600

    Updates to support Gcloud

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -13,7 +13,7 @@ export default function BookItem({book, onBookDelete, onBookUpdate, userRole}){
                               </div> 
                               <div className='card-footer'>
                                 <button className='btn btn-danger' onClick={(evt) => onBookDelete(evt, book._id)}>Delete</button>
-                                {userRole.includes('quality analyst') && <Link to={`/books/update/${book._id}`} className='btn btn-info'>Update</Link>}
+                                {/* {userRole.includes('quality analyst') && <Link to={`/books/update/${book._id}`} className='btn btn-info'>Update</Link>} */}
                                </div> 
                         </div>
         </div>
